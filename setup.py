e43f3f54aac20d0bb50b931b9fdac25db15f6806
Ankur Pandey <matrixbegins@gmail.com>
2021-04-08 11:26:58 +0530

    changing version number of package in setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
         name ='roksopsql',
-        version ='0.2.2',
+        version ='0.2.4',
         author ='Ankur Pandey',
         author_email ='matrixbegins@gmail.com',
         url ='https://github.com/matrixbegins/rokso-pgsql',
